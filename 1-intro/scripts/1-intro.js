043150587c8c46cdbe47676c63228d0a2f0fb96b
taras55555 <tkaminskyi@gmail.com>
2023-06-14 19:51:22 +0300

    Working on task #3 1. Created variables 'course' and 'group' and assigned them values. 2. Printed their values to the console and displayed them to the user in the 'main__text' element. 3. Copied the value of the 'group' variable into the 'course' variable. 4. Repeated step 2.

--- 1-intro/scripts/1-intro.js
+++ 1-intro/scripts/1-intro.js
@@ -1,2 +1,17 @@
 // 2
 console.log("Kaminskyi");
+
+// 3
+let course;
+let group;
+const valuseOfVariables = document.querySelector(".main__text");
+
+course = "JavaScript Fundamentals";
+group = "0906";
+
+console.log(course + " " + group);
+valuseOfVariables.innerHTML = course + " " + group;
+
+course = group;
+console.log(course + " " + group);
+valuseOfVariables.innerHTML = valuseOfVariables.innerHTML + "<br>" + course + " " + group;
