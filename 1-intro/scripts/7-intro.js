6a17922a6ddf58e9d9a9cb9a79a8d5cce0b3c42f
taras55555 <tkaminskyi@gmail.com>
2023-06-15 21:14:32 +0300

    Created js 7-intro Task #7

---
+++ 1-intro/scripts/7-intro.js
@@ -0,0 +1,5 @@
+const login = window.prompt("Enter yout username");
+const email = window.prompt("Enter yout email");
+const password = window.prompt("Enter yout password");
+
+console.log("Dear " + login + ", your email is " + email + ", your password is " + password);
--- 1-intro/scripts/index.js
+++ 1-intro/scripts/index.js
@@ -1,2 +1,3 @@
 import("./1-intro.js");
 import("./6-intro.js");
+import("./7-intro.js");
