e3ec822156b4a648bb8dd64391456777711bf9e1
taras55555 <tkaminskyi@gmail.com>
2023-06-16 14:19:57 +0300

    Added feature. The field cannot be empty. (Task #7)

--- 1-intro/scripts/7-intro.js
+++ 1-intro/scripts/7-intro.js
@@ -3,9 +3,21 @@ const runTaskButton = document.querySelector("#taskSeven");
 
 runTaskButton.addEventListener('click', function () {
     console.log("Task #7");
-    const login = window.prompt("Enter your username");
-    const email = window.prompt("Enter your email");
-    const password = window.prompt("Enter your password");
+
+    let login = window.prompt("Enter your username");
+    while(login === "" || login === null) {
+        login = window.prompt("The field cannot be empty. Enter your username");
+    }
+
+    let email = window.prompt("Enter your email");
+    while(email === "" || email === null) {
+        email = window.prompt("The field cannot be empty. Enter your email");
+    }
+
+    let password = window.prompt("Enter your password");
+    while(password === "" || password === null) {
+        password = window.prompt("The field cannot be empty. Enter your password");
+    }
 
     alert("Dear " + login + "\nyour email is " + email + ",\nyour password is " + password);
 });
