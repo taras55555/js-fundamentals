8e2a75be94d0cdcad045b589c5b8b2c3077eb3cd
taras55555 <tkaminskyi@gmail.com>
2023-06-16 13:07:08 +0300

    Added alert  message

--- 1-intro/scripts/2-intro.js
+++ 1-intro/scripts/2-intro.js
@@ -4,4 +4,5 @@ const runTaskButton = document.querySelector("#taskTwo");
 runTaskButton.addEventListener('click', function () {
     console.log("Task #2");
     console.log("Kaminskyi");
+    alert("Check the console");
 });
