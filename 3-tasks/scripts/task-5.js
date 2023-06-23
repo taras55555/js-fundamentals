3a0027b5b001e76beafc499a1392ddac489ec58d
taras55555 <tkaminskyi@gmail.com>
2023-06-23 21:42:44 +0300

    Topic #3. Task #5 Implemented solution

---
+++ 3-tasks/scripts/task-5.js
@@ -0,0 +1,30 @@
+// Task #5
+const runTaskButton = document.querySelector("#taskFive");
+
+runTaskButton.addEventListener('click', function () {
+    let arr = [5, "Limit", 12, "a", "3", 99, 2, [2, 4, 3, "33", "a", "text"], "strong", "broun"];
+    // let arr = ["null", 5, "Limit", [null, undefined], 12, "a", "3", 99, 2, [2, 4, 3, "33", "a", "text"], [true, NaN, NaN], "strong", "broun", true];
+    let arrNew = funcName(arr);
+    console.log(arr);
+    console.log(arrNew);
+});
+
+function funcName(arr) {
+    let newArr = [];
+    for (const element of arr.flat(Infinity)) {
+        let iterator = 0;
+        let isSuccess = false;
+        for (let newValue of newArr) {
+            if (typeof newValue[0] === typeof element) {
+                newArr[iterator].push(element);
+                isSuccess = true;
+                break;
+            }
+            iterator++;
+        }
+        if (!isSuccess) {
+            newArr.push([element]);
+        }
+    }
+    return newArr;
+}
