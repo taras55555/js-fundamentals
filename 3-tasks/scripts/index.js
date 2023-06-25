6d8bd63e693a4fcb86839983f1431eef7aefb9b3
taras55555 <tkaminskyi@gmail.com>
2023-06-25 12:02:47 +0300

    Topic #3. Task #7 implementation solution

--- 3-tasks/scripts/index.js
+++ 3-tasks/scripts/index.js
@@ -4,3 +4,4 @@ import ("./task-3.js");
 import ("./task-4.js");
 import ("./task-5.js");
 import ("./task-6.js");
+import ("./task-7.js");
---
+++ 3-tasks/scripts/task-7.js
@@ -0,0 +1,19 @@
+// Task #7
+const runTaskButton = document.querySelector("#taskSeven");
+
+runTaskButton.addEventListener('click', function () {
+    findUnique([1, 2, 3, 5, 3]);
+    findUnique([1, 2, 3, 5, 11]);
+});
+
+function findUnique(arr) {
+    let isUnique = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.indexOf(arr[i]) !== i) {
+            isUnique = false;
+            break;
+        }
+    }
+    console.log(isUnique);
+    return isUnique;
+}
