d99f31c5df7153dcd3a0c672a63e5b6f1b62f844
taras55555 <tkaminskyi@gmail.com>
2023-06-28 19:58:31 +0300

    Allowed decimal numbers as function parameters

--- 4-tasks/scripts/task-3.js
+++ 4-tasks/scripts/task-3.js
@@ -3,6 +3,7 @@ const runTaskButton = document.querySelector("#taskThree");
 
 runTaskButton.addEventListener('click', function () {
     console.log(calcRectangleArea("0.1", 5));
+    console.log(calcRectangleArea(0.1, 5));
     console.log(calcRectangleArea(0, 5));
     console.log(calcRectangleArea(0.555, 66));
     console.log(calcRectangleArea(1, 5));
@@ -13,10 +14,10 @@ runTaskButton.addEventListener('click', function () {
 function calcRectangleArea(width, height) {
     let rectangleArea = null;
     try {
-        if (!Number.isInteger(width)) throw new TypeError("Please check the width\nThe value \"" + width + "\" is not allowed");
-        if (!Number.isInteger(height)) throw new TypeError("Please check the height\nThe value \"" + height + "\" is not allowed");
+        if (typeof width !== "number") throw new TypeError("Please check the width\nThe value \"" + width + "\" is not allowed");
+        if (typeof height !== "number") throw new TypeError("Please check the height\nThe value \"" + height + "\" is not allowed");
         if (width <= 0 || height <= 0) throw new RangeError("Please check the ranges. The values must be greater than zero");
-        const rectangleArea = width * height;
+        rectangleArea = width * height;
         return rectangleArea; 
     }
     catch (error) {
