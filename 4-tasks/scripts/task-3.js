0cb5ac3952ddb182a261a978db45d8ffc5ef4a80
taras55555 <tkaminskyi@gmail.com>
2023-06-30 01:40:41 +0300

    Topic #4. Task #3 Refactored code
    Task #4
    Implemented solution

--- 4-tasks/scripts/task-3.js
+++ 4-tasks/scripts/task-3.js
@@ -3,26 +3,24 @@ const runTaskButton = document.querySelector("#taskThree");
 
 runTaskButton.addEventListener('click', function () {
     console.log("Task #3");
-    console.log(calcRectangleArea("0.1", 5));
-    console.log(calcRectangleArea(0.1, 5));
-    console.log(calcRectangleArea(0, 5));
-    console.log(calcRectangleArea(0.555, 66));
-    console.log(calcRectangleArea(1, 5));
-    console.log(calcRectangleArea(12, 5));
-    console.log(calcRectangleArea(1, 5));
+    try {
+        const width = 7;
+        const height = 5;
+        alert("Rectangle area is " + width + " * " + height + " = " + calcRectangleArea(width, height));
+    }
+    catch (error) {
+        alert(error);
+        console.log("Incorrect data");
+    }
 });
 
 function calcRectangleArea(width, height) {
-    let rectangleArea = null;
     try {
         if (typeof width !== "number") throw new TypeError("Please check the width\nThe value \"" + width + "\" is not allowed");
         if (typeof height !== "number") throw new TypeError("Please check the height\nThe value \"" + height + "\" is not allowed");
         if (width <= 0 || height <= 0) throw new RangeError("Please check the ranges. The values must be greater than zero");
-        rectangleArea = width * height;
-        return rectangleArea; 
-    }
-    catch (error) {
-        alert(error.name + "\n" + error.message);
-        return rectangleArea;   
-    }
+    } catch (error) {
+        throw error;
+    };
+    return width * height;
 }
--- 4-tasks/scripts/task-4.js
+++ 4-tasks/scripts/task-4.js
@@ -3,5 +3,40 @@ const runTaskButton = document.querySelector("#taskFour");
 
 runTaskButton.addEventListener('click', function () {
     console.log("Task #4");
-    alert("The solution for this task is currently unavailable.");
+    try {
+        console.log(showMonthName(1));
+        console.log(showMonthName(2));
+        console.log(showMonthName(3));
+        console.log(showMonthName(4));
+        console.log(showMonthName(5));
+        console.log(showMonthName(6));
+        console.log(showMonthName(7));
+        console.log(showMonthName(8));
+        console.log(showMonthName(9));
+        console.log(showMonthName(10));
+        console.log(showMonthName(11));
+        console.log(showMonthName(12));
+        console.log(showMonthName(55));
+
+    } catch (error) {
+        if (error instanceof MonthException) {
+            console.log(error.name + " " + error.message);
+        } else {
+            console.log("Unexpected error: " + error);
+        }
+    }
 });
+
+class MonthException {
+    constructor(message) {
+        this.name = "MonthException";
+        this.message = message;
+    }
+}
+function showMonthName(month) {
+    if (month < 1 || month > 12) throw new MonthException("Incorrect month number");
+    const date = new Date();
+    date.setMonth(--month, 1);
+    const options = { month: "long" };
+    return date.toLocaleString("en-US", options);
+}
