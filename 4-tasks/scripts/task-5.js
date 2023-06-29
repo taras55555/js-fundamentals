ea7b43448a66050128b25262f54058c5d7f7c42e
taras55555 <tkaminskyi@gmail.com>
2023-06-30 02:13:34 +0300

    Topic #4 Implemented solution for Task #5

--- 4-tasks/scripts/task-5.js
+++ 4-tasks/scripts/task-5.js
@@ -3,5 +3,29 @@ const runTaskButton = document.querySelector("#taskFive");
 
 runTaskButton.addEventListener('click', function () {
     console.log("Task #5");
-    alert("The solution for this task is currently unavailable.");
+    const userIds = [7, -12, 44, -22];
+    // const userIds = [7, 0, 44, 22];
+    const userList = showUsers(userIds);
+    console.log(userList);
+    alert("Check the console");
 });
+
+function showUser(id) {
+    if (id < 0) {
+        throw new RangeError('Invalid user ID. ID must be a positive number.');
+    }
+    return { id };
+}
+
+function showUsers(ids) {
+    const users = [];
+    for (let id of ids) {
+        try {
+            const user = showUser(id);
+            users.push(user);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    return users;
+}
