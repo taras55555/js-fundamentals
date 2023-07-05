28efb81e91627ddaa6e57ba69964abaaf2854353
taras55555 <tkaminskyi@gmail.com>
2023-07-05 16:25:32 +0300

    Created templates. Topic #6

---
+++ 8-tasks/scripts/task-3.js
@@ -0,0 +1,7 @@
+// Task #3
+const runTaskButton = document.querySelector("#taskThree");
+
+runTaskButton.addEventListener('click', function () {
+    console.log("Task #3");
+    alert("The solution for this task is currently unavailable.");
+});
---
+++ 8-tasks/scripts/task-6.js
@@ -0,0 +1,7 @@
+// Task #6
+const runTaskButton = document.querySelector("#taskSix");
+
+runTaskButton.addEventListener('click', function () {
+    console.log("Task #6");
+    alert("The solution for this task is currently unavailable.");
+});
