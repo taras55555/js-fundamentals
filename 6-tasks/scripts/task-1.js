e92676facf4d32e753f5700aaac6347dc89b9197
taras55555 <tkaminskyi@gmail.com>
2023-07-06 16:42:29 +0300

    Implemented solution for Topic #6 Task #1

--- 6-tasks/scripts/task-1.js
+++ 6-tasks/scripts/task-1.js
@@ -3,5 +3,15 @@ const runTaskButton = document.querySelector("#taskOne");
 
 runTaskButton.addEventListener('click', function () {
     console.log("Task #1");
-    alert("The solution for this task is currently unavailable.");
+    const ulElements = document.querySelectorAll("#list li");
+    const resultElement = document.querySelector("#task1Result");
+    const resultArray = [];
+    const arrayUlElements = Array.from(ulElements);
+    const lengthList = arrayUlElements.length;
+
+    for (let i = 0; i < lengthList; ++i) {
+        let currentElement = i%2===0 ? arrayUlElements.shift() : arrayUlElements.pop();    
+        resultArray.push(currentElement.textContent);
+    }
+    resultElement.innerHTML = resultArray.join(', ');
 });
