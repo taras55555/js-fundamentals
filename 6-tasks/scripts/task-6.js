f85ca0eb7d56344d956735bc80abecd0ef36ad05
taras55555 <tkaminskyi@gmail.com>
2023-07-07 12:07:07 +0300

    Code refactoring -changed files structure -added template for task #6

--- 6-tasks/scripts/task-6.js
+++ 6-tasks/scripts/task-6.js
@@ -1,7 +1,1 @@
 // Task #6
-const runTaskButton = document.querySelector("#taskSix");
-
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #6");
-    alert("The solution for this task is currently unavailable.");
-});
