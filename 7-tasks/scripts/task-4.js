3e32c53ad5ccbe2ceb879d123c640239e56ba41c
taras55555 <tkaminskyi@gmail.com>
2023-07-09 15:42:08 +0300

    Solution for task #4

--- 7-tasks/scripts/task-4.js
+++ 7-tasks/scripts/task-4.js
@@ -1,7 +1,13 @@
 // Task #4
-const runTaskButton = document.querySelector("#taskFour");
-
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #4");
-    alert("The solution for this task is currently unavailable.");
+deleteOption.addEventListener('click', () => {
+    try {
+        if (persons.value === '') throw new ReferenceError("There are no any persons");
+        const currentPerson = persons.querySelector("option[value=" + persons.value + "]");
+        currentPerson.remove();
+        if (persons.length === 0) throw new ReferenceError("That was the last person");
+    } catch (e) {
+        alert(e);
+    } finally {
+        if (persons.length === 0) persons.previousSibling.previousSibling.textContent = "There are no any persons";
+    }
 });
