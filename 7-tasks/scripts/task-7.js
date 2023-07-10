21366b572d17e74a827dd3cf13ea6eaa6191c9c6
taras55555 <tkaminskyi@gmail.com>
2023-07-10 15:43:31 +0300

    It was bad solution

--- 7-tasks/scripts/task-7.js
+++ 7-tasks/scripts/task-7.js
@@ -11,74 +11,25 @@ const cityList = {
     ukr: ["Cherkasy", "Chernihiv", "Chernivtsi", "Dnipro", "Donetsk", "Ivano-Frankivsk", "Kharkiv", "Kherson", "Khmelnytskyi", "Kropyvnytskyi", "Kyiv", "Luhansk", "Lutsk", "Lviv", "Mykolaiv", "Odesa", "Poltava", "Rivne", "Simferopol", "Sumy", "Ternopil", "Uzhhorod", "Vinnytsia", "Zaporizhzhia", "Zhytomyr"]
 }
 
-class selectListCountries {
-    constructor(id, options) {
-        this.id = id;
-        this.options = options;
+class SelectList {
+    constructor(name) {
+        this.name = name;
         this.listSelect = document.createElement("select");
     }
-    createSelectListCountries() {
+
+    createSelectList() {
         this.listSelect.name = this.id;
         this.listSelect.id = this.id;
         this.listSelect.classList.add("select-list");
 
-        for (let key in this.options) {
-            let currentElement = document.createElement("option");
-            currentElement.value = key;
-            currentElement.text = this.options[key];
-            this.listSelect.add(currentElement);
-        }
         taskSeven.appendChild(this.listSelect)
     }
-    dropList() {
-        this.listSelect.parentNode.removeChild(this.listSelect);
-    }
 }
 
-class selectListCities extends selectListCountries {
-    constructor(id, options) {
-        super(id, options);
-    }
-    setOptions(newOptions) {
-        this.options = newOptions;
-    }
+const nameCountriesList = "countries"
+const nameCitiesList = "cities"
 
-    createSelectListCities() {
-        const countryKey = document.querySelector("#countries").value;
-        const citiesInCountry = {};
-        for (const index in this.options[countryKey]) {
-            citiesInCountry[parseInt(index) + 1] = this.options[countryKey][index];
-        }
-        this.setOptions(citiesInCountry);
-        this.createSelectListCountries();
-    }
-}
-
-const countries = new selectListCountries("countries", countryList)
-countries.createSelectListCountries()
-
-let cities = new selectListCities("cities", cityList)
-cities.createSelectListCities()
-
-const countryElement = document.querySelector("#countries");
-let cityElement;
-
-countryElement.addEventListener('change', updateCountry);
-
-updateCountry();
-
-function updateCountry() {
-    cities.dropList();
-    cities = new selectListCities("cities", cityList)
-    cities.createSelectListCities();
-    cityElement = document.querySelector("#cities");
-    cityElement.addEventListener('change', updateCity);
-    updateCity();
-}
-
-function updateCity() {
-    cityElement = document.querySelector("#cities");
-    const selectedOptionCity = cityElement.querySelector("option[value='" + cityElement.value + "']");
-    const selectedOptionCountry = countryElement.querySelector("option[value='" + countryElement.value + "']");
-    taskSevenText.textContent = `${selectedOptionCountry.textContent}, ${selectedOptionCity.textContent}`;
-}
+const contriesList = new SelectList(nameCountriesList);
+contriesList.createSelectList();
+const citiesList = new SelectList(nameCitiesList);
+citiesList.createSelectList();
