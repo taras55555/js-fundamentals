943a51ade0cf646487da983c461f1911a051fd40
taras55555 <tkaminskyi@gmail.com>
2023-07-11 00:22:51 +0300

    Finished Task #7

--- 7-tasks/scripts/task-7.js
+++ 7-tasks/scripts/task-7.js
@@ -12,24 +12,79 @@ const cityList = {
 }
 
 class SelectList {
-    constructor(name) {
+    setOptionsList(newOptions) { this.optionsList = newOptions; }
+
+    getOptionsList() { return this.optionsList; }
+
+    appendSelectList() {
+        const [[firstKey, firstValue]] = Object.entries(this.optionsList);
+        if (Array.isArray(firstValue)) {
+            this.extractArray();
+            this.buildOptionElements();
+        } else {
+            this.buildOptionElements();
+        }
+    }
+
+    buildOptionElements() {
+        for (let key in this.getOptionsList()) {
+            const currentOption = document.createElement("option");
+            currentOption.value = key;
+            currentOption.text = this.getOptionsList()[key];
+            this.listSelect.add(currentOption);
+        }
+        taskSeven.appendChild(this.listSelect);
+    }
+    extractArray() {
+        const countryKey = document.querySelector("#countries").value
+        const objectCities = {};
+        for (let val in this.getOptionsList()[countryKey]) {
+            objectCities[parseInt(val) + 1] = this.getOptionsList()[countryKey][val];
+        }
+        this.setOptionsList(objectCities);
+    }
+    clearOptions(cityList) {
+        while (this.listSelect.firstChild) {
+            this.listSelect.remove(this.listSelect.firstChild);
+        }
+        this.setOptionsList(cityList);
+        this.appendSelectList();
+        this.printInfo();
+    }
+    printInfo() {
+        const countryPrint = countries.options[countries.selectedIndex].textContent;
+        const cityPrint = cities.options[cities.selectedIndex].textContent;
+        taskSevenText.textContent = `${countryPrint}, ${cityPrint}`;
+    }
+}
+
+class FillingContryiesSelect extends SelectList {
+    constructor(name, optionsList) {
+        super();
         this.name = name;
+        this.optionsList = optionsList;
         this.listSelect = document.createElement("select");
     }
 
     createSelectList() {
-        this.listSelect.name = this.id;
-        this.listSelect.id = this.id;
+        this.listSelect.name = this.name;
+        this.listSelect.id = this.name;
         this.listSelect.classList.add("select-list");
-
-        taskSeven.appendChild(this.listSelect)
+        super.appendSelectList();
     }
 }
 
-const nameCountriesList = "countries"
-const nameCitiesList = "cities"
+const nameCountriesList = "countries";
+const nameCitiesList = "cities";
 
-const contriesList = new SelectList(nameCountriesList);
+const contriesList = new FillingContryiesSelect(nameCountriesList, countryList);
 contriesList.createSelectList();
-const citiesList = new SelectList(nameCitiesList);
+const citiesList = new FillingContryiesSelect(nameCitiesList, cityList);
 citiesList.createSelectList();
+citiesList.printInfo();
+countries.addEventListener('change', () => {
+    citiesList.clearOptions(cityList);
+});
+cities.addEventListener('change', () => {
+    citiesList.printInfo();
+});
