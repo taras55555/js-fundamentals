2e05db022702fead310e530151a36093ab56174c
taras55555 <tkaminskyi@gmail.com>
2023-07-11 23:29:10 +0300

    Solution for Task #2 Added preload image Updated styles

--- 8-tasks/scripts/task-2.js
+++ 8-tasks/scripts/task-2.js
@@ -1,7 +1,22 @@
 // Task #2
 const runTaskButton = document.querySelector("#taskTwo");
+let currentTimeout;
 
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #2");
-    alert("The solution for this task is currently unavailable.");
+runTaskButton.addEventListener('keyup', () => {
+    resultTaskTwo.textContent = "";
+    resultTaskTwo.classList.add("image-container");
+    clearTimeout(currentTimeout);
+    currentTimeout = setTimeout(() => {
+        const currentValue = taskTwo.value;
+        resultTaskTwo.textContent = checkEmail(currentValue);
+        resultTaskTwo.classList.remove("image-container");
+    }, 1000);
 });
+
+function checkEmail(currentText) {
+    if (!currentText) return `String's empty`
+    const regExpression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
+    const searching = regExpression.test(currentText.trim());
+    console.log(searching);
+    return searching;
+}
