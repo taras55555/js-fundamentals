de483e6adf2ea9adf025900b66338808003f15c7
taras55555 <tkaminskyi@gmail.com>
2023-07-11 13:48:56 +0300

    Implemented solution for Task #2

--- 8-tasks/scripts/task-1.js
+++ 8-tasks/scripts/task-1.js
@@ -1,7 +1,15 @@
 // Task #1
 const runTaskButton = document.querySelector("#taskOne");
 
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #1");
-    alert("The solution for this task is currently unavailable.");
+runTaskButton.addEventListener('keyup', function () {
+    const currentValue = taskOne.value;
+    searchResult.innerHTML = upperCase(currentValue);
 });
+
+function upperCase(currentText) {
+    if(!currentText) return `String's empty`
+    const regExpression = /^[A-Z]/g;
+    // const searching = regExpression.test(currentText);
+    const searching = regExpression.test(currentText.trim());
+    return searching ? `String's <span class="green-text">starts</span> with uppercase character` : `String's <span class="red-text">not</span> starts with uppercase character`;
+}
