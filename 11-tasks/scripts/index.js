34730f775294baf6b7eff2a3440b458bcd6e63d9
taras55555 <tkaminskyi@gmail.com>
2023-07-25 15:49:48 +0300

    Create templates

--- 11-tasks/scripts/index.js
+++ 11-tasks/scripts/index.js
@@ -1,6 +1,4 @@
-import ("./task-1.js");
-import ("./task-2.js");
-import ("./task-3.js");
-import ("./task-4.js");
-import ("./task-5.js");
-import ("./task-6.js");
+import("./task-1.js");
+import("./task-2.js");
+import("./task-3.js");
+import("./task-4.js");
--- 12-tasks/scripts/task-1.js
+++ 12-tasks/scripts/task-1.js
@@ -3,4 +3,5 @@ const runTaskButton = document.querySelector("#taskOne");
 
 runTaskButton.addEventListener('click', function () {
     console.log("Task #1");
-})
+    alert("The solution for this task is currently unavailable.");
+});
