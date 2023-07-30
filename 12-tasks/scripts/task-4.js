d86099f5e88b8011720ddaaa68d858b8f9a73311
Тарас <tkaminskyi@gmail.com>
2023-07-30 23:08:27 +0300

    Created template Task #12

--- 12-tasks/scripts/task-4.js
+++
@@ -1,7 +0,0 @@
-// Task #4
-const runTaskButton = document.querySelector("#taskFour");
-
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #4");
-    alert("The solution for this task is currently unavailable.");
-});
--- 12-tasks/scripts/task-5.js
+++
@@ -1,7 +0,0 @@
-// Task #5
-const runTaskButton = document.querySelector("#taskFive");
-
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #5");
-    alert("The solution for this task is currently unavailable."); 
-});
