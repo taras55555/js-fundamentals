0529717df2bda314a478df2deb10b3da05dd2cf0
taras55555 <tkaminskyi@gmail.com>
2023-07-31 11:52:46 +0300

    Fixed

--- 11-tasks/scripts/task-1.js
+++ 11-tasks/scripts/task-1.js
@@ -1,20 +1,16 @@
-// Task #1
+runTaskButton.addEventListener('click', function () {
+    console.log("Task #1");
+    function getPromise(text, delay) {
+        return new Promise(function (resolve) {
+            setTimeout(() => resolve(text), delay);
+        })
+    }
 
-console.log("Task #1");
+    getPromise("test promise", 2000).then(function (data) {
+        console.log(data);
+    });
+    getPromise("test next promise", 10000).then(function (data) {
+        console.log(data);
+    });
 
-const http = require('http');
-const os = require('os');
-const path = require('path');
-const port = 5000;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    const userInfo = os.userInfo().username;
-    const osType = os.type();
-    const osUptime = (os.uptime() / 60).toFixed(2);
-    const filename = path.basename(__filename);
-    const dirName = process.cwd();
-
-    const data = 'Current user name: ' + userInfo + '\nOS type: ' + osType + '\nSystem work time: ' + osUptime + ' minutes\nCurrent work directory: ' + dirName + '\nServer file name: ' + filename;
-
-    res.end(data);
-}).listen(port);
+});
