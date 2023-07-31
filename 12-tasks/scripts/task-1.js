abfc7fe050ff112959c4121f73543884df4126d1
taras55555 <tkaminskyi@gmail.com>
2023-07-31 11:51:00 +0300

    Implemented solution for Task #1

--- 12-tasks/scripts/task-1.js
+++ 12-tasks/scripts/task-1.js
@@ -1,7 +1,20 @@
 // Task #1
-const runTaskButton = document.querySelector("#taskOne");
 
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #1");
-    alert("The solution for this task is currently unavailable.");
-});
+console.log("Task #1");
+
+const http = require('http');
+const os = require('os');
+const path = require('path');
+const port = 5000;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    const userInfo = os.userInfo().username;
+    const osType = os.type();
+    const osUptime = (os.uptime() / 60).toFixed(2);
+    const filename = path.basename(__filename);
+    const dirName = process.cwd();
+
+    const data = 'Current user name: ' + userInfo + '\nOS type: ' + osType + '\nSystem work time: ' + osUptime + ' minutes\nCurrent work directory: ' + dirName + '\nServer file name: ' + filename;
+
+    res.end(data);
+}).listen(port);
