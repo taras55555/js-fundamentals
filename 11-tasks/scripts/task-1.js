d2c643a02275456335f55b0adfab96907d921342
taras55555 <tkaminskyi@gmail.com>
2023-07-31 11:46:36 +0300

    Implemented solution for Task #1

--- 11-tasks/scripts/task-1.js
+++ 11-tasks/scripts/task-1.js
@@ -1,20 +1,20 @@
 // Task #1
-const runTaskButton = document.querySelector("#taskOne");
 
-runTaskButton.addEventListener('click', function () {
-    console.log("Task #1");
+console.log("Task #1");
 
-    function getPromise(text, delay) {
-        return new Promise(function (resolve) {
-            setTimeout(() => resolve(text), delay);
-        })
-    }
+const http = require('http');
+const os = require('os');
+const path = require('path');
+const port = 5000;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    const userInfo = os.userInfo().username;
+    const osType = os.type();
+    const osUptime = (os.uptime() / 60).toFixed(2);
+    const filename = path.basename(__filename);
+    const dirName = process.cwd();
 
-    getPromise("test promise", 2000).then(function (data) {
-        console.log(data);
-    });
-    getPromise("test next promise", 10000).then(function (data) {
-        console.log(data);
-    });
-
-});
+    const data = 'Current user name: ' + userInfo + '\nOS type: ' + osType + '\nSystem work time: ' + osUptime + ' minutes\nCurrent work directory: ' + dirName + '\nServer file name: ' + filename;
+    // const combinedDataJSON = JSON.stringify(userInfo, "<br>",osType);
+    res.end(data);
+}).listen(port);
